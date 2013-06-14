e2f8d15bcf59fc337a1b7824a250ab76c0d60034
Paul Frazee <pfrazee@gmail.com>
2013-06-14 13:56:32 -0500

    added link headers

--- index.php
+++ index.php
@@ -11,13 +11,17 @@ $protocol = basename($_GET['id']);
 if ($protocol == '') $protocol = 'index';
 
 $type = getBestSupportedMimeType(array('text/html','text/plain'));
-$spec = @file_get_contents('_'.$protocol.'.md');
+$links = @file_get_contents('_'.$protocol.'.links');
 
+$spec = @file_get_contents('_'.$protocol.'.md');
 if ($spec == FALSE) {
   header("HTTP/1.1 404 not found");
   echo "not found";
   die;
 }
+
+if ($links)
+   header('Link: ' . $links);
 
 if ($type == 'text/html') {
   header('Content-type: text/html');
